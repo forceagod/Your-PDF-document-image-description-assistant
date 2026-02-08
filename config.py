14d68f7fd50fba7669cef450117f927c90bde150
forceagod <136326114+forceagod@users.noreply.github.com>
2026-02-08 14:14:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-chat_model_api_key = "sk-c8f09fecf95a49f2b7b1456e7fb5f3e9"
-v_model_api_key = "8f7affaca409488c918b013459f7952c.8AjXQWudy32y6VK6"
+chat_model_api_key = "your api key"
+
+v_model_api_key = "your api key"
