41d5732f9203163339e0113a4ac209636783d612
forceagod <136326114+forceagod@users.noreply.github.com>
2026-02-08 14:13:23 +0800

    Update state.py

--- state.py
+++ state.py
@@ -9,46 +9,6 @@ from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
 
 
-###################
-# Structured Outputs
-###################
-class judge_out(BaseModel):
-    reason_for_Judgment:str = Field(
-        description="Reasons for judging whether the image is meaningful",
-    )
-    is_meaningful: bool = Field(
-        description="Whether the image is meaningful.",
-    )
-
-class disc_image_out(BaseModel):
-    """this model for disc a image"""
-    need_info: bool = Field(
-        description="Whether the information is insufficient to describe the image",
-    )
-    image_disc: str = Field(
-        description="the discription of image",
-    )
-
-class summmary_image_info_from_context_out(BaseModel):
-    """this model for summary info from context for given image"""
-    need_extract: bool = Field(
-        description="Whether the information is insufficient to describe the image",
-    )
-    image_info:str = Field(
-        description="the info extracted from context base on the given image",
-    )
-
-
-###################
-# State Definitions
-###################
-
-def override_reducer(current_value, new_value):
-    """Reducer function that allows overriding values in state."""
-    if isinstance(new_value, dict) and new_value.get("type") == "override":
-        return new_value.get("value", new_value)
-    else:
-        return operator.add(current_value, new_value)
     
 class AgentInputState(TypedDict):
     """InputState is only 'messages'."""
@@ -69,5 +29,6 @@ class AgentState(TypedDict):
     Number_of_check: int = 0
     final_image_disc:str
     p_error:str
+
 
 
